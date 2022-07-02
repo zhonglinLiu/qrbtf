df360545b82452604d76040740370fd3cc1351c9
zhonglin.liu <hi.life@qq.com>
2022-07-02 18:09:23 +0800

    for me

--- src/components/app/PartFooter.js
+++ src/components/app/PartFooter.js
@@ -10,38 +10,29 @@ const PartFooter = () => (
             <div className="Qr-footer-part">
                 <strong>作者</strong>&emsp;
                 <LinkTrace
-                    href="https://blog.ciaochaos.com/"
+                    href="https://www.zhihu.com/people/cai-niao-82-86"
                     rel="noopener noreferrer"
                     target="_blank">ciaochaos
                 </LinkTrace>&emsp;
-                <LinkTrace
-                    href="https://github.com/CPunisher/"
-                    rel="noopener noreferrer"
-                    target="_blank">CPunisher
-                </LinkTrace>
                 <span className="gray">&ensp;丨&ensp;</span>
-                <span><LinkTrace href="https://www.yuque.com/qrbtf/docs/privacy" rel="noopener noreferrer"
+                {/* <span><LinkTrace href="https://www.yuque.com/qrbtf/docs/privacy" rel="noopener noreferrer"
                                  target="_blank">隐私政策</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
                 <span><LinkTrace href="https://www.yuque.com/qrbtf/docs/terms" rel="noopener noreferrer"
-                                 target="_blank">使用条款</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="https://www.yuque.com/qrbtf/docs/contact" rel="noopener noreferrer" target="_blank">
+                                 target="_blank">使用条款</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span> */}
+                <span><LinkTrace href="mailto:hi.life@qq.com?subject=qrCode" rel="noopener noreferrer" target="_blank">
                     联系我们</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="https://www.yuque.com/qrbtf/docs/api" rel="noopener noreferrer" target="_blank">
-                    API 接口 <sup>测试</sup></LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="https://www.yuque.com/qrbtf/docs/coop"
-                                 rel="noopener noreferrer" target="_blank">合作咨询</LinkTrace></span>
             </div>
 
             <div className="Qr-footer-part">
                 <strong>更多产品</strong>&emsp;
-                <span><LinkTrace href="https://mdnice.com" rel="noopener noreferrer"
-                         target="_blank">mdnice 公众号排版</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="https://urlify.cn/" rel="noopener noreferrer"
-                         target="_blank">Urlify 短链接</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="https://imgkr.com/" rel="noopener noreferrer"
-                         target="_blank">imgkr 图壳图床</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="https://blog.ciaochaos.com/mpmath/"
-                         rel="noopener noreferrer" target="_blank">mpMath 公众号公式插件</LinkTrace></span>
+                <span><LinkTrace href="https://mango.wehere.top/tools/" rel="noopener noreferrer"
+                         target="_blank">在线工具箱</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
+                <span><LinkTrace href="https://app.wehere.top/" rel="noopener noreferrer"
+                         target="_blank">淘宝优惠券</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
+                <span><LinkTrace href="https://mango.wehere.top/tools/markdown" rel="noopener noreferrer"
+                         target="_blank">markdown编辑器</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
+                <span><LinkTrace href="https://app.wehere.top/periodic-table/index.html"
+                         rel="noopener noreferrer" target="_blank">元素周期表</LinkTrace></span>
             </div>
 
             <div className="Qr-footer-part">
@@ -50,50 +41,13 @@ const PartFooter = () => (
                                  target="_blank">Sangmin, Shim</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
                 <span><LinkTrace href="https://github.com/cozmo/jsQR" rel="noopener noreferrer"
                                  target="_blank">Cosmo Wolfe</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="https://mp.weixin.qq.com/s/61kI-2TmxNza1U9tw-MNPA" rel="noopener noreferrer"
-                                 target="_blank">董斯佳</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="https://mp.weixin.qq.com/s/qCTnkPWEX6AfL2CJua_AqQ" rel="noopener noreferrer"
-                                 target="_blank">野生符号</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="https://github.com/kongxiangyan" rel="noopener noreferrer"
-                                 target="_blank">Cigaret</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="https://mp.weixin.qq.com/s/NdVsF_FJxqSZOyGionoo1Q" rel="noopener noreferrer"
-                                 target="_blank">JaBi 扎比</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="http://nav.6soluo.com/" rel="noopener noreferrer"
-                                 target="_blank">一为</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="https://reactjsexample.com/a-simple-web-app-to-beautify-your-qr-code/" rel="noopener noreferrer"
-                                 target="_blank">React.js Example</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="https://mp.weixin.qq.com/s/J-odC4GPd9N2V7QRhEJ23g" rel="noopener noreferrer"
-                                 target="_blank">木子淇</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="https://github.com/gexin1/beautify-qrcode" rel="noopener noreferrer"
-                                 target="_blank">River</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="https://mp.weixin.qq.com/s/yy7knjp9MJ3LEuMF42gtqw" rel="noopener noreferrer"
-                                 target="_blank">编程如画</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="https://mp.weixin.qq.com/s/YWStNIYUZ7UmcHGhIwE6eg" rel="noopener noreferrer"
-                                 target="_blank">JZ Creative</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="https://mp.weixin.qq.com/s/5r3PNDSaQb3sOXtDQt3Jnw" rel="noopener noreferrer"
-                                 target="_blank">工具狂人</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="https://sspai.com/post/61118" rel="noopener noreferrer"
-                                 target="_blank">少数派</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="https://www.creative-tim.com/blog/web-development/best-reactjs-project-examples/" rel="noopener noreferrer"
-                                 target="_blank">Creative Tim</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="https://mp.weixin.qq.com/s/bg69nfB0MK8_bd4yEErxIA" rel="noopener noreferrer"
-                                 target="_blank">PPT 进化论</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="https://mp.weixin.qq.com/s/AoxOxZcBmo_1FK71CHviGQ" rel="noopener noreferrer"
-                                 target="_blank">旁门左道</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="https://www.iplaysoft.com/qrbtf.html" rel="noopener noreferrer"
-                                 target="_blank">异次元</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="https://mp.weixin.qq.com/s/zSzH8WilPsACmF1K2cWPVA" rel="noopener noreferrer"
-                                 target="_blank">Topbook</LinkTrace><span className="gray">&ensp;丨&ensp;</span></span>
-                <span><LinkTrace href="https://www.v2ex.com/t/675982"
-                                 rel="noopener noreferrer" target="_blank">V2EX</LinkTrace></span>
             </div>
 
             <div className="Gray">
-                Copyright © {currentYear} QRBTF. 保留所有权利。<br/>
+                Copyright © {currentYear} hi.life@qq.com. 保留所有权利。<br/>
                 <LinkTrace href="http://www.beian.miit.gov.cn/"
                    rel="noopener noreferrer" target="_blank">
-                    浙
-                    ICP 备 19005869 号
+                    豫ICP备17025106号
                 </LinkTrace>
             </div>
         </div>
--- src/components/app/PartMore.js
+++ src/components/app/PartMore.js
@@ -24,8 +24,6 @@ const PartMore = () => {
     return (
         <div className="Qr-titled-nobg">
             <div className="Qr-Centered title-margin">
-                <div className="Qr-s-title">More</div>
-                <p className="Qr-s-subtitle">更多</p>
                 <div className="Qr-s-subtitle Qr-rel">
                     {isPC() ? <div className="Qr-style-hint">拖拽滑动</div> : null}
                 </div>
@@ -33,10 +31,6 @@ const PartMore = () => {
             <div className="title-margin">
                 <div className="Qr-article">
                     <div className="Qr-Centered">
-                        <p><GitHubButton href="https://github.com/ciaochaos/qrbtf"
-                                         data-color-scheme="no-preference: light; light: light; dark: dark;"
-                                         data-icon="octicon-star" data-size="large" data-show-count="true"
-                                         aria-label="Star ciaochaos/qrbtf on GitHub">Star</GitHubButton></p>
                         <h2>设计分享</h2>
                     </div>
                     <ScrollContainer
@@ -50,28 +44,6 @@ const PartMore = () => {
                         </div>
                     </ScrollContainer>
                     <div className="Qr-Centered">
-                        <h2>最新消息</h2>
-                        <p><b>2020.9.1</b><br/>新增 C3 样式、图标插入、 PNG 下载。</p>
-                        <p><b>2020.6.29</b><br/>新的反馈渠道！我们开始征集好玩的二维码设计啦，可以是推送尾图、海报等等，快来上传吧。<LinkTrace
-                            href='https://qrbtf-com.mikecrm.com/J2wjEEq' rel="noopener noreferrer"
-                            target="_blank">点击提交</LinkTrace>。</p>
-                        <p><b>2020.6.24</b><br/>新增深色模式，新增 SP — 3 样式。</p>
-                        <p><b>2020.6.22</b><br/>新增 A — a1、A — a2、A — b1、A — b2 样式，添加样式描述，新增桌面端横向拖拽滑动。</p>
-                        <p><b>2020.6.11</b><br/>QRBTF React 组件发布！快在你的应用中引入 react-qrbtf 吧。<LinkTrace
-                            href='https://github.com/cpunisher/react-qrbtf' rel="noopener noreferrer"
-                            target="_blank">访问项目</LinkTrace>。</p>
-                        <p><b>2020.5.23</b><br/>网站开源啦！<LinkTrace href='https://github.com/ciaochaos/qrbtf'
-                                                                 rel="noopener noreferrer"
-                                                                 target="_blank">点击访问</LinkTrace> 我们的
-                            Github。期待你也来参与设计、开发！给个
-                            Star 支持一下不？查看文章 <LinkTrace href='https://mp.weixin.qq.com/s/GFEMCWQu3e2qhTuBabnHmQ'
-                                                       rel="noopener noreferrer" target="_blank">QRBTF
-                                开源啦！来写个二维码样式吧～</LinkTrace>。</p>
-                        <p><b>2020.5.22</b><br/>新增 C2 样式、下载计数（好像还不太稳定）、普通二维码上传、颜色选择、背景图片上传、输入提示、桌面端横向滚动提示。</p>
-                        <h2>为什么要做一个二维码生成器？</h2>
-                        <p>看这里，<LinkTrace href='https://mp.weixin.qq.com/s/_Oy9I9FqPXhfwN9IUhf6_g'
-                                          rel="noopener noreferrer"
-                                          target="_blank">如何制作一个漂亮的二维码</LinkTrace> 这篇文章简要介绍了我们的初心与愿景。</p>
                         <h2>这个生成器的特别之处在哪里？</h2>
                         <p>普通的二维码样式单一，不能与环境较好的融合。这一个生成器有着 <b>丰富的参数化样式、基于 SVG 的二维码生成能力</b>，在为我们提供精美样式的同时，不限制参数如数值、颜色、背景图片的选择，又因
                             SVG 有较好的拓展性，可以完美兼容矢量制图流程。</p>
@@ -84,22 +56,16 @@ const PartMore = () => {
                         <h2>二维码无法识别的原因是什么？</h2>
                         <p>二维码无法识别有很多原因。比如定位点不匹配识别模式、信息点颜色对比不够、遮挡部分太大。建议尝试调整容错率、颜色、图标大小等参数并在各种二维码扫描器中测试，以保证二维码被识别的成功率</p>
                         <h2>使用遇到了问题，怎么反馈？</h2>
-                        <p>我们是两位大一的学生，忙于学业，可能在设计与开发的过程中有一些疏漏，敬请谅解。如果遇到浏览器兼容问题，请暂时选择更换软件或设备尝试。</p>
-                        <p>编写二维码样式是一个锻炼设计与开发（JavaScript）能力的绝佳机会，如果你有兴趣和我们一起玩这个项目，欢迎添加我的微信（微信号：<span
-                            style={{userSelect: "text"}}>nhciao</span>，请备注真实姓名）或发送邮件至 <LinkTrace
-                            href='mailto:contact@qrbtf.com'>contact@qrbtf.com</LinkTrace> 联系我们！</p>
+                        <p>发送邮件至 <LinkTrace
+                            href='mailto:hi.life@qq.com'>hi.life@qq.com</LinkTrace> 联系我！</p>
 
                     </div>
                 </div>
             </div>
             <div className="Qr-Centered btn-row">
                 <div className="div-btn">
-                    <LinkButton href={"https://www.yuque.com/qrbtf/docs/donate"} value={"打赏"}/>
-                    <LinkButton href={"https://github.com/ciaochaos/qrbtf"} value={"Github"}/>
-                </div>
-                <div className="div-btn">
-                    <LinkButton href={"https://qrbtf-com.mikecrm.com/J2wjEEq"} value={"反馈"}/>
-                    <LinkButton href={"https://mp.weixin.qq.com/s/GFEMCWQu3e2qhTuBabnHmQ"} value={"开发"}/>
+                    <LinkButton href={"https://dun.mianbaoduo.com/@bean"} value={"打赏"}/>
+                    <LinkButton href={"https://app.wehere.top/"} value={"优惠券"}/>
                 </div>
             </div>
         </div>
